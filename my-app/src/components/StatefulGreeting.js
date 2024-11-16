a0e3c4021e9e4a069eda493897b2d607ba55a7c0
Taiwo Ogbonyomi <taiwoogbonyomi@gmail.com>
2024-11-16 22:40:46 +0000

    Update the state with prev

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -11,31 +11,33 @@ constructor(props) {
 }
 
 handleClick() {
-    this.setState({
-        introduction: this.state.introduction === "Hello!" ? "Goodbye!" : "Hello!",
-        buttonText: this.state.buttonText === "Exit" ? "Enter" : "Exit",
-    }, ()=> {
-        console.log('new state',this.state.introduction);
-        console.log('new state',this.state.buttonText);
+    this.setState((prevState, prevProps) => {
+        console.log('Previous State:', prevState)
+        console.log('Previous Props:', prevProps);
+        return {
+            introduction: prevState.introduction === "Hello!" ? "Goodbye!" : "Hello!",
+            buttonText: prevState.buttonText === "Exit" ? "Enter" : "Exit",
+        };
     });
-    console.log(this.state.introduction);
-    console.log(this.state.buttonText);
-
 }
 
 increment() {
-    this.setState({
-        count: this.state.count + 1
-    })
-    console.log(this.state.count);
+    this.setState((prevState, prevProps) => {
+       console.log('Previous State:', prevState)
+       console.log('Previous Props:', prevProps);
+       return {
+        count: prevState.count + 1,
+        };
+    });
+   
 }
 
 incrementFive(){
-    this.increment()
-    this.increment()
-    this.increment()
-    this.increment()
-    this.increment()
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
+    this.increment();
 }
 
     render () {
@@ -47,7 +49,7 @@ incrementFive(){
                 <p>You've clicked {this.state.count} times</p>
             </div>
         ) 
-    }
+    };
 }
 
 export default StatefulGreeting;
